28aa3884c6bf7366e7390b328622d780671b05f2
João Vitor <jvitor52@yahoo.com.br>
2025-01-01 14:58:02 -0300

    port enb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import app from './app';
 
-const appPort = process.env.APP_PORT || 3000;
+const appPort = process.env.PORT || 3000;
 const appEnv = process.env.APP_ENV || 'local';
 
 app.listen(appPort, async () => {
   console.log(
     `🚀 Server is running at port ${appPort}, environment "${appEnv}"`
   );
-})
+});
